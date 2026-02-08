1cca35b3c47cf8bef56a87d64700ffb7af13ebdb
vankhaipro <vankhai26022004@gmail.com>
2026-02-08 14:13:39 +0700

    feat:user.entity.ts

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { User } from "@prisma/client";
 import { ApiProperty } from "@nestjs/swagger";
 import { Exclude } from "class-transformer";
-import { UserRole } from "@prisma/client";
+import { UserRole, TeamRole } from "@prisma/client";
 
 export class UserEntity implements User {
   id: string;
@@ -23,6 +23,9 @@ export class UserEntity implements User {
 
   @ApiProperty({ enum: UserRole })
   role: UserRole;
+
+  @ApiProperty({ enum: TeamRole, nullable: true, required: false })
+  team_role: TeamRole | null;
 
   manager_id: string | null;
   is_active: boolean;
